5a2078e64a48a903e4743b8afa121f0aa9d199ca
Jakob Pennington <jakobpennington@gmail.com>
2018-07-20 10:40:03 +0930

    Reapplied some reverted changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: 'hibp',
-        loadChildren: 'app/hibp/hibp.module#HIBPModule'
-    },
+    // {
+    //     path: 'hibp',
+    //     loadChildren: 'app/hibp/hibp.module#HIBPModule'
+    // },
     {
         path: '',
         loadChildren: 'app/landing/landing.module#LandingModule'
